622b6e0ccffc227245bc130b92c9043fc98b463a
GK Sir <hellofcode@hell.you>
2021-07-20 20:44:31 +0530

    Tue20Jul202108:44:31PM+0530

---
+++ RelationalOperator.cpp
@@ -0,0 +1,20 @@
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+
+	int a,b;
+
+	cout<<"Enter 2 numbers : ";
+	cin>>a>>b;
+
+	 if(a>b) cout<<a<<" is greater than "<<b<<endl;
+	if(a>=b) cout<<a<<" is greater than or equals to"<<b<<endl;
+	 if(a<b) cout<<a<<" is less than "<<b<<endl;
+	if(a<=b) cout<<a<<" is less than or equals to "<<b<<endl;
+	if(a==b) cout<<a<<" is equals to "<<b<<endl;
+	if(a!=b) cout<<a<<" is not equals to "<<b<<endl;
+
+}
