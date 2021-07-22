d29fbdac25e14d3598582d8df8d1b3253400d114
GK Sir <hellofcode@hell.you>
2021-07-22 21:17:37 +0530

    Thu22Jul202109:17:37PM+0530

---
+++ ConditionalCode.cpp
@@ -0,0 +1,12 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+       //t   ||  f
+	// -----------------
+	if(!'\0' || NULL)
+		cout<<"me";
+	else
+		cout<<"python";
+}
---
+++ ExpressionCode.cpp
@@ -0,0 +1,17 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+
+	int a = 31<<4-3; //3>8
+
+	/**
+	 * c = a*2^b
+	 *   = 31*2^1
+	 *   = 31*2
+	 *   =62
+	 * */
+
+	cout<<"value of a : "<<a<<endl;
+}
