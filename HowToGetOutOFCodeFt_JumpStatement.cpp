2f559124d749bf43a0af52964088803b5408d1cb
GK Sir <hellofcode@hell.you>
2021-07-26 21:09:00 +0530

    Mon26Jul202109:09:00PM+0530

--- HowToGetOutOFCodeFt_JumpStatement.cpp
+++ HowToGetOutOFCodeFt_JumpStatement.cpp
@@ -1,18 +1,32 @@
 
 /**
- * A :  Break : loop exit
+ * A :  Break : loop exit // continue for skiping iteration
  * B : Return : function exit
  * C :   exit : program exit
  * */
 #include<iostream>
 using namespace std;
+int say()
+{
+		cout<<"statement 1"<<endl;
+			// return 10;
+		exit(0);
+		cout<<"statement 2"<<endl;
+
+}
 int main()
 {
 
-	for(int i=0;i<10;i++)
-	{
-		if(i!=5) break;
-		cout<<"line "<<i<<endl;
-	}
+	// for(int i=0;i<10;i++)
+	// {
+	// 	if(i%2==0)
+	// 	{ 
+	// 		break;
+	// 	}
+	// 		cout<<"line "<<i<<endl;
+	// }
+	cout<<"-------------------Code 1"<<endl;
+			say();
+	cout<<"-------------------Code 2"<<endl;
 
 }
