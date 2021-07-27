1e3b13158f33a705ad6753c0817bd8c8540c3a18
GK Sir <hellofcode@hell.you>
2021-07-27 20:54:55 +0530

    Tue27Jul202108:54:54PM+0530

--- ArrayInCpp.cpp
+++ ArrayInCpp.cpp
@@ -2,9 +2,25 @@
 #include<iostream>
 using namespace std;
 int main()
-{            //0 1 2 3 4 5 6 7 8 9
-	int a[] = {1,2,3,4,5,6,7,8,9,0};
+{              //0 1 2 3 4 5 6 7 8 9
+	 int a[100];
+	 int i,n;
 
+	 cout<<"Enter the size : ";
+	 cin>>n;
 
-	cout<<"data : "<<a[3]<<endl;
+	 cout<<"Enter the array : "<<endl;
+
+	 for(i=0;i<n;i++)
+	 {
+	 	cout<<" a[ "<<i<<" ] = ";
+	 	cin>>a[i];
+	 }
+
+	 cout<<"\n----------------------------\n";
+	 for(i=0;i<n;i++)
+	 {
+	 	cout<<" a[ "<<i<<" ] = "<<a[i]<<endl;
+	 }
+
 }
