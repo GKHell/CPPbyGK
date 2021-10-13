88d003f29ced6b26d05c26a9b85940e0e5a0e700
GkHell <gpkmyworld@gmail.com>
2021-10-13 20:03:10 +0530

    13/10/2021⠀⎦⎡⠀20:03⠀⎦⎢⠀Wednesday

---
+++ KP_TASK/P06_Q01_ANS_Vehicle.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Vehicle{
+
+protected:
+	int vehicleno,classisno;
+public:
+	virtual void getData() 
+	{
+		cout<<"Enter the vehicle no : ";
+		cin>>vehicleno;
+
+		cout<<"Enter the classis no : ";
+		cin>>classisno;
+	}
+
+	virtual void showData()
+	{
+		cout<<"\nVehicle no : "<<vehicleno<<", classis no : "<<classisno<<endl;
+	}
+
+};
+
+class Scooter : public Vehicle{
+
+private:
+	string company;
+	int wheels;
+
+public :
+	void getData()
+	{
+		cout<<"Enter the vehicle no : ";
+		cin>>vehicleno;
+
+		cout<<"Enter the classis no : ";
+		cin>>classisno;
+
+		cout<<"Enter wheels : ";
+		cin>>wheels;
+
+		cout<<"Enter company : ";
+		cin>>company;
+	}
+
+    void showData()
+	{
+		cout<<"\nVehicle no : "<<vehicleno<<", classis no : "<<classisno
+		<<", wheels : "<<wheels<<", company : "<<company<<endl;
+	}
+
+};
+
+class Code : public Vehicle{
+
+private:
+	double price;
+	string company;
+
+public:
+	void getData()
+	{
+		cout<<"Enter the vehicle no : ";
+		cin>>vehicleno;
+
+		cout<<"Enter the classis no : ";
+		cin>>classisno;
+
+		cout<<"Enter price : ";
+		cin>>price;
+
+		cout<<"Enter company : ";
+		cin>>company;
+	}
+
+    void showData()
+	{
+		cout<<"\nVehicle no : "<<vehicleno<<", classis no : "<<classisno<<", price : "<<price<<", company : "<<company<<endl;
+	}
+
+};
+
+int main()
+{
+	Vehicle *ptr;
+	Scooter s;
+	Code me;
+
+	ptr = &s;
+		ptr->getData();
+		ptr->showData();
+
+	cout<<"\n----------------------------\n\n";
+
+	ptr = &me;
+		ptr->getData();
+		ptr->showData();
+
+}
