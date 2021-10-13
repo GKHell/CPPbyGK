7a28e0b85defa6290238b73eae021ad18dbc277d
GkHell <gpkmyworld@gmail.com>
2021-10-13 23:27:49 +0530

    13/10/2021⠀⎦⎡⠀23:27⠀⎦⎢⠀Wednesday

--- KP_TASK/P07_Q03_ANS_FILE.cpp
+++ KP_TASK/P07_Q03_ANS_FILE.cpp
@@ -22,5 +22,18 @@ int main()
 	file.put('H');
 
 	file.seekg(0,ios::beg);
-	cout<<"get() : "<<file.get()<<endl;
+	cout<<"get() : "<<char(file.get())<<endl;
+
+	string data;
+
+	cout<<"enter a line : ";
+	cin>>data;
+
+	file<<data;
+
+	file.seekg(0,ios::beg);
+	string newData;
+	getline(file,newData);
+
+	cout<<"getline ; "<<newData<<endl;
 }
